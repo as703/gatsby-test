41e2f454849f31ff94e2841c81270d1c0374be2f
Alasdair Scott <dr.aj.scott@gmail.com>
2020-02-09 11:25:46 +0000

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,48 @@
-import React from "react"
+import * as React from 'react'
+import { graphql} from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
 
-export default () => <div>Hello world!</div>
+export const query = graphql`
+  query {
+    mobileHero: file(relativePath: { eq: "mobileHero.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth:800, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    desktopHero: file(relativePath: { eq: "desktopHero.png" }) {
+      childImageSharp {
+        fluid(maxWidth:1600, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+export default ({data}) => {
+
+  const sources = [
+    {
+      ...data.mobileHero.childImageSharp.fluid,
+      media: `(max-width: 767px)`,
+    },
+    {
+      ...data.desktopHero.childImageSharp.fluid,
+      media: `(min-width: 768px)`,
+    },
+  ];
+
+  return (
+    <div>
+
+    <BackgroundImage fluid={sources} style={{width:'100vw',
+                                            height:'500px',
+                                            backgroundPosition: 'left top'}}/>
+
+
+    </div>
+
+  )
+}
